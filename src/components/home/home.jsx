f7b119839816b190e70a4394f5e1c58d7c1b612d
cocobutte <cocobutter128@gmail.com>
2022-11-12 15:37:37 +0500

    updated commt

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -6,14 +6,39 @@ import Button from '@mui/material/Button';
 import {getAuth,createUserWithEmailAndPassword,onAuthStateChanged, signOut  } from "firebase/auth"
 import { useNavigate } from 'react-router-dom';
 import Modal from '@mui/material/Modal';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as React from 'react';
 import AddIcon from '@mui/icons-material/Add';
-
-
-
+import { FormControl } from '@mui/material';
 import "./home.css"
 import { async } from '@firebase/util';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc,getDocs,doc, onSnapshot,query,serverTimestamp
+        , orderBy, limit, deleteDoc,updateDoc,getDoc
+      } 
+ from "firebase/firestore"; 
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+
+ const firebaseConfig = {
+        apiKey: "AIzaSyAdiCIB2HUruPfENbArNu0yaqBceTHPGbc",
+        authDomain: "attendence-portal-24e5f.firebaseapp.com",
+        projectId: "attendence-portal-24e5f",
+        storageBucket: "attendence-portal-24e5f.appspot.com",
+        messagingSenderId: "573317723957",
+        appId: "1:573317723957:web:5c2f9594edad9c107abe7c",
+        measurementId: "G-31M3TYD022"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+const auth = getAuth(app);
+
 
 
 
@@ -28,11 +53,7 @@ function Home (){
     const [classSec, setClassSec] = useState(null)
     const [classCourse, setClassCourse] = useState(null)
     const [batch, setBatch] = useState(null)
-
-
-
-    
-
+    const [classes, setClasses] = useState()
 
 
 
@@ -56,22 +77,78 @@ function Home (){
 
     const submitHandler = async (e) =>{
         e.preventDefault()
+        if(classCourse && classSec && classSchedule && classTeacher && batch && classTiming  !== null) {
         try {
-            const docRef = await addDoc(collection(db, "classes"), {
-              text: values.text,
-              img : res?.data?.url,
-              userProfileImg : auth?.currentUser?.photoURL,
-              user:auth?.currentUser?.displayName,
-              date: serverTimestamp()
+            const docRef = await addDoc(collection(db, "classes"),{
+              Class: classCourse,
+              section : classSec,
+              Schedule : classSchedule,
+              Teacher : classTeacher,
+              Batch :batch,
+              ClassTiming : classTiming
 
             });
             console.log("Document written with ID: ", docRef.id);
           } catch (e) {
             console.error("Error adding document: ", e);
           }
-
-
+        }
+        else{
+            alert("Fill out All fields First")
+      
+          
+        }
     }
+
+    // const deletePost = async (postId) => {
+    //     await deleteDoc(doc(db, "classes", postId))
+    
+    //   }
+
+    useEffect(() =>{
+
+        const getData = async () => {
+          const querySnapshot = await getDocs(collection(db, "classes"));
+          querySnapshot.forEach((doc) => {
+          console.log(`${doc.id} => `,doc.data());
+          setClasses((prev) =>{
+            let newArray = [...prev,doc.data()  ]
+            return(newArray)
+      
+          })
+        });
+      
+        }
+        // getData();
+        let unsubscribe = null
+        const realTimeData = async () =>{
+          const q = query(collection(db, "classes"), orderBy("desc"));
+          unsubscribe = onSnapshot(q, (querySnapshot) => {
+          const posts = [];
+          querySnapshot.forEach((doc) => {
+            posts.push({id:doc.id, ...doc.data()});
+            console.log(posts)
+        });
+      
+        // if (posts.length !== 0 ) {
+            console.log("Post", posts); 
+            setClasses(posts)
+      
+      
+          
+        // }
+        });
+        }
+      
+        realTimeData();
+        return () =>{
+          console.log("Clean up")
+          unsubscribe();
+        }
+        
+      },[])
+
+          
 
 
 
@@ -100,14 +177,20 @@ function Home (){
                                     Add Class
                                 </Typography>
                                 <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                                    <form onAbort={submitHandler}>
-                                        <input type="text" placeholder='Enter Class Timing'/> <br />
-                                                                                
-                                        <select class="form-select form-select-sm" aria-label=".form-select-sm example">
-                                            <option selected>Select Class Schedule</option>
-                                            <option value="1">Monday, Wednesday, Friday(M.W.S)</option>
-                                            <option value="2">Tuesday, Thursday, Saturday(T.T.S)</option>
-                                        </select> <br />
+                                    
+                                        <input type="text" placeholder='Enter Class Timing'
+                                        onChange={(e) =>{
+                                            setClassTiming(e.target.value)
+                                            
+                                        }}/> <br />
+
+                                        <input type="text" placeholder='Enter Class Days'
+                                        onChange={(e) =>{
+                                            setClassSchedule(e.target.value)
+                                            
+                                        }}/> <br />
+        
+
                                         <input type="text" placeholder="Enter Teacher Name" onChange={(e) =>{
                                             setClassTeacher(e.target.value)
                                             
@@ -124,13 +207,8 @@ function Home (){
                                             setBatch(e.target.value)
                                             
                                         }} />  <br />
-                                        <Button variant="contained" type='submit' onClick={handleClose} startIcon={<AddIcon />}>Add</Button>
-
-
-
-
+                                        <Button variant="contained" type='submit' onClick={submitHandler} startIcon={<AddIcon />}>Add</Button>
                                         
-                                    </form>
                                 </Typography>
                                 </Box>
                         </Modal>
@@ -138,6 +216,22 @@ function Home (){
                     </AppBar>
                 </Box>
             </div>
+
+            <div className='displayClass'>
+                {
+                         classes.map((eachPost,i) => (
+                         <div>
+                            <p>{eachPost.Batch}</p>
+
+                         </div>
+
+                         ))
+                }
+                
+
+                
+            </div>
+
 
       
 
